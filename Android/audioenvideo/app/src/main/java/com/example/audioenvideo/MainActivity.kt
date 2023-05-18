7ded039636e86a995b792c4ed93ced5e04986963
JonathanDeCatelle <79463699+JonathanDeCatelle@users.noreply.github.com>
2023-05-18 19:28:58 +0200

    new android appl. with views

---
+++ Android/audioenvideo/app/src/main/java/com/example/audioenvideo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.audioenvideo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
