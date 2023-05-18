a9cb68846fc3773935ccc81d1c7f5c822cbfeb54
JonathanDeCatelle <79463699+JonathanDeCatelle@users.noreply.github.com>
2023-05-18 19:31:22 +0200

    format weg

--- React native/audioenvideo/src/App.tsx
+++ React native/audioenvideo/src/App.tsx
@@ -1,15 +1,8 @@
-/**
- * AudioEnVideo applicatie
- * Bachelorproef Jonathan De Catelle 2022-2023
- *
- * @format
- */
-
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 import React from 'react';
 
-function App(): JSX.Element {
+const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>AudioEnVideo</Text>
