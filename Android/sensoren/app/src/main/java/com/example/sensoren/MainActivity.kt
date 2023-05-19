d338ad766a8777ad04f2b1da107842b86e8da69a
JonathanDeCatelle <79463699+JonathanDeCatelle@users.noreply.github.com>
2023-05-19 22:35:32 +0200

    sensoren

--- Android/sensoren/app/src/main/java/com/example/sensoren/MainActivity.kt
+++ Android/sensoren/app/src/main/java/com/example/sensoren/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.sensoren
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.perf.FirebasePerformance
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+    private var gyroscope: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtAccelerometer = findViewById<TextView>(R.id.txtAccelerometer)
+        val txtGyroscoop = findViewById<TextView>(R.id.txtGyroscoop)
+        val btnAccelerometer = findViewById<Button>(R.id.btnGetAccelerometer)
+        val btnGyroscoop = findViewById<Button>(R.id.btnGetGyroscoop)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
+        val sensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                val myTrace = FirebasePerformance.getInstance().newTrace("getDataGyroscoop")
+
+                myTrace.start()
+                try {
+                    if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                        val x = event.values[0]
+                        val y = event.values[1]
+                        val z = event.values[2]
+
+                        runOnUiThread{
+                            txtAccelerometer.text = "x=$x, y=$y, z=$z"
+                        }
+                    } else if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+                        val x = event.values[0]
+                        val y = event.values[1]
+                        val z = event.values[2]
+
+                        runOnUiThread{
+                            txtGyroscoop.text = "x=$x, y=$y, z=$z"
+                        }
+                    }
+                } finally {
+                    myTrace.stop()
+                }
+
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                return
+            }
+        }
+
+        btnAccelerometer.setOnClickListener {
+            sensorManager.registerListener(
+                sensorEventListener,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+
+        btnGyroscoop.setOnClickListener {
+            sensorManager.registerListener(
+                sensorEventListener,
+                gyroscope,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
     }
 }
--- React native/sensoren/src/App.tsx
+++ React native/sensoren/src/App.tsx
@@ -1,18 +1,87 @@
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SensorTypes, accelerometer, gyroscope, setUpdateIntervalForType } from 'react-native-sensors';
 
-import React from 'react';
+import perf from '@react-native-firebase/perf';
+
+setUpdateIntervalForType(SensorTypes.accelerometer, 10);
+setUpdateIntervalForType(SensorTypes.gyroscope, 10);
 
 const App = () => {
+  const [accelerometerData, setAccelerometerData] = useState({});
+  const [gyroscopeData, setGyroscopeData] = useState({});
+  
+  const accelerometerTrace = perf().newTrace('accelerometer3333');
+  const gyroscopeTrace = perf().newTrace('gyroscope3333');
+  
+  const handleAccelerometerData = ({ x, y, z, timestamp }) => {
+    accelerometerTrace.start();
+    setAccelerometerData({ x, y, z, timestamp });
+    accelerometerTrace.stop();
+  };
+  
+  const handleGyroscopeData = ({ x, y, z, timestamp }) => {
+    gyroscopeTrace.start();
+    setGyroscopeData({ x, y, z, timestamp });
+    gyroscopeTrace.stop();
+  };
+  
+  useEffect(() => {
+    const accelerometerSubscription = accelerometer.subscribe(handleAccelerometerData);
+    
+    return () => {
+      accelerometerSubscription.unsubscribe();
+      accelerometerTrace.stop(); // Stop de trace wanneer de component wordt gedemonteerd
+    };
+  }, []);
+  
+  useEffect(() => {
+    const gyroscopeSubscription = gyroscope.subscribe(handleGyroscopeData);
+    
+    return () => {
+      gyroscopeSubscription.unsubscribe();
+      gyroscopeTrace.stop(); // Stop de trace wanneer de component wordt gedemonteerd
+    };
+  }, []);
+  
+  const getAccelerometerData = () => {
+    // Implementeer hier de gewenste logica voor het ophalen van accelerometergegevens
+  };
+  
+  const getGyroscopeData = () => {
+    // Implementeer hier de gewenste logica voor het ophalen van gyroscoopgegevens
+  };
+  
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Sensoren</Text>
+      <Text style={styles.title}>Data sensoren opvragen</Text>
+      <View style={styles.dataContainer}>
+        <Text>Accelerometer data:</Text>
+        <Text>{JSON.stringify(accelerometerData)}</Text>
+      </View>
+      <Button title="Accelerometer" onPress={getAccelerometerData} />
+      <View style={styles.dataContainer}>
+        <Text>Gyroscoop data:</Text>
+        <Text>{JSON.stringify(gyroscopeData)}</Text>
+      </View>
+      <Button title="Gyroscoop" onPress={getGyroscopeData} />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  dataContainer: {
+    flexDirection: 'row',
+    marginVertical: 10,
   },
 });
 
